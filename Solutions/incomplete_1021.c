09f833eafa150b581281bc60407c605964995129
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2025-10-20 01:22:32 +0600

    Refactor currency calculation logic in incomplete_1021.c

--- Solutions/incomplete_1021.c
+++ Solutions/incomplete_1021.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 int main()
 {
-    float N;
-    scanf("%f", &N);
+    float x;
+    scanf("%f",&x);
+    int a;
     printf("NOTAS:\n");
-    int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s;
-    a = N/100;
-    printf("%d nota(s) de R$ 100.00\n", a);
-    b = N-(a*100);
-    c = b/50;
-    printf("%d nota(s) de R$ 50.00\n", c);
-    d = b-(c*50);
-    e = d/20;
-    printf("%d nota(s) de R$ 20.00\n", e);
-    f = d-(e*20);
-    g = f/10;
-    printf("%d nota(s) de R$ 10.00\n", g);
-    h = f-(g*10);
-    i = h/5;
-    printf("%d nota(s) de R$ 5.00\n", i);
-    j = h-(i*5);
-    k = j/2;
-    printf("%d nota(s) de R$ 2.00\n", k);
+    a=x/100;
+    printf("%d nota(s) de R$ 100.00\n",a);
+    x=x-(a*100);
+    a=x/50;
+    printf("%d nota(s) de R$ 50.00\n",a);
+    x=x-(a*50);
+    a=x/20;
+    printf("%d nota(s) de R$ 20.00\n",a);
+    x=x-(a*20);
+    a=x/10;
+    printf("%d nota(s) de R$ 10.00\n",a);
+    x=x-(a*10);
+    a=x/5;
+    printf("%d nota(s) de R$ 5.00\n",a);
+    x=x-(a*5);
+    a=x/2;
+    printf("%d nota(s) de R$ 2.00\n",a);
     printf("MOEDAS:\n");
-    l = j-(k*2);
-    m = l/1;
-    printf("%d moeda(s) de R$ 1.00\n", m);
-    n = l-(m*1);
-    o = n*2;
-    printf("%d moeda(s) de R$ 0.50\n", o);
+    x=x-(a*2);
+    a=x/1;
+    printf("%d moeda(s) de R$ 1.00\n",a);
+    float b;
+    x=x-(a*1);
+    b=x * 2;
+    printf("%.0f moeda(s) de R$ 0.50\n",b);
+    x=x-(b*0.5);
+    b=x*4;
+    printf("%.0f moeda(s) de R$ 0.25\n",b);
+    x=x-(b*0.25);
+    b=x*10;
+    printf("%.0f moeda(s) de R$ 0.10\n",b);
+    x=x-(b*0.10);
+    b=x*20;
+    printf("%.0f moeda(s) de R$ 0.05\n",b);
+    x=x-(b*0.05);
+    b=x*100;
+    printf("%.0f moeda(s) de R$ 0.01\n",b);
 
-    printf("%d", l);
-    
+
+
     return 0;
 }
