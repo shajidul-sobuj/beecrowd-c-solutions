e22366424e90a67a0811dcc87039e66e13f22180
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2025-10-20 12:57:51 +0600

    Add salary tax calculation program
    Initial implementation of salary tax calculation.

---
+++ Solutions/incomplete_1051.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    float salary;
+    scanf("%d",&salary);
+    if (salary >=0 && salary <= 2000.00){
+        printf("Isento\n");
+    }else if (salary >2000.00 && salary <= 3000.00){
+        salary=salary-2000.00;
+        tax=salary*0.08;
+        prinf("R$ %0.2f\n")''
+    }else if (salary > && salary <= 2000.00)
+ 
+    return 0;
+}
