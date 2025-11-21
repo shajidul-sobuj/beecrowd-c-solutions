443ea0f1d3fde692435dd7e34bea535a6ad83d8b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2025-11-21 16:44:56 +0600

    Update 2755_solution.c

--- Solutions/2755_solution.c
+++ Solutions/2755_solution.c
@@ -4,7 +4,7 @@ int main() {
 
 	char c;
 	c='"';
-	printf("%cRo'b'er        to\\/%c\n",c,c);
+	printf("%cRo'b'er\tto\\/%c\n",c,c);
 	printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
 	printf("(^_-) (-_-) (-_^)\n");
 	printf("(%c_%c) ('.')\n",c,c);
